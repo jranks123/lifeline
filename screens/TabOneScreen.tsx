f1c90c4682f0e3a4eb94d2f54f5d16149453b6ff
J E Rankin <jonny@Js-MacBook-Pro.local>
2022-05-04 14:53:10 +0100

    add sad and death svg

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -3,13 +3,15 @@
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
-import DeathSvg from '../assets/images/DeathSvg'
+import DeathSvg from '../assets/images/DeathSvg';
+import SadSvg from '../assets/images/SadSvg';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Onde</Text>
-      <DeathSvg  />
+      <SadSvg />
+      <DeathSvg />
     </View>
   );
 }

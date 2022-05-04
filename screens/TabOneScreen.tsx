a0be2bea68358719b17086336a2fbd131c102daf
J E Rankin <jonny@Js-MacBook-Pro.local>
2022-05-04 14:30:14 +0100

    initial commit

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,15 @@
-import { StyleSheet } from 'react-native';
+  import { StyleSheet, Image   } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import DeathSvg from '../assets/images/DeathSvg'
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Text style={styles.title}>Tab Onde</Text>
+      <DeathSvg />
     </View>
   );
 }
